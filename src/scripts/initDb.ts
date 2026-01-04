33e8ef5cb869b9c8f8f34e7192348f07342e7d21
Jim Haastrup <37276210+Jimrealf@users.noreply.github.com>
2026-01-04 17:08:29 -0500

    Add PostgreSQL database configuration and initialization scripts

---
+++ src/scripts/initDb.ts
@@ -0,0 +1,34 @@
+import { pool } from '../config/db';
+
+const createTables = async () => {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                base_currency VARCHAR(3) NOT NULL,
+                target_currency VARCHAR(3) NOT NULL,
+                rate DECIMAL(10, 4) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        `)
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS favorites (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER REFERENCES users(id),
+                base_currency VARCHAR(3) NOT NULL,
+                target_currency VARCHAR(3) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            `)
+        console.log('Tables created successfully');
+    } catch (error) {
+        console.error('Error creating tables:', error);
+        throw error;
+    } finally {
+        pool.end();
+    }
+}
+
+createTables();
